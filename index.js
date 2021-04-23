bb123dca14ff602fd0ea837800389626cedb3638
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-23 16:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ function decrement(n) {
 }
 
 function makeInt(n) {
-  parseInt(n, 10)
+  parseInt(`n`, 10)
   return n
 }
 
 function preserveDecimal(n) {
-  parseFloat(n)
+  parseFloat(`n`)
   return n
 }
